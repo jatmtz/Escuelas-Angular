463e5ff097064b698686ec52f0b679c03ef6362b
jatmtz <jatmtz1215@gmail.com>
2024-03-12 16:42:37 -0600

    Resolved merge conflict in app.config.ts2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,9 +6,5 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-<<<<<<< HEAD
   providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
-=======
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withInterceptors([customInterceptor])), provideAnimationsAsync()]
->>>>>>> origin/main
 };
